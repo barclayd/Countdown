68f66ca0409588fd03312a2a663cb5415fff3483
Daniel Barclay <daniel@urbanintel.io>
2018-11-02 21:38:13 +0000

    Added logic to check if countdown has completed and animate cards logic

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-let updateTimer = (deadline) => {
+const updateTimer = (deadline) => {
     // calculates time left until deadline 
     const time = deadline - new Date();
     return {
@@ -6,8 +6,16 @@ let updateTimer = (deadline) => {
         'hours': Math.floor((time / (1000 * 60 * 60)) % 24),
         'minutes': Math.floor((time / (1000 * 60)) % 60),
         'seconds': Math.floor((time / (1000)) % 60),
-        'time': time
+        'total': time
     };
+}
+
+const animateClock = (span) => {
+    // animation lasts for 0.7 seconds
+    span.className = 'turn';
+    setTimeout(() => {
+        span.className = '';
+    }, 700);
 }
 
 const startTimer = (id, deadline) => {
@@ -21,6 +29,20 @@ const startTimer = (id, deadline) => {
             '<span>' + timer.hours + '</span>' +
             '<span>' + timer.minutes + '</span>' +
             '<span>' + timer.seconds + '</span>';
+
+        const spans = clock.getElementsByTagName("span");
+        animateClock(spans[3]);
+        if (timer.seconds == 59) animateClock(spans[2]);
+        if (timer.minutes == 59 && timer.seconds == 59) animateClock(spans[1]);
+        if (timer.minutes == 23 && timer.minutes == 59 && timer.seconds == 59) animateClock(spans[0]);
+
+
+        // check if deadline has passed
+        if (timer.total < 1) {
+            clearInterval(timeInterval);
+            clock.innerHTML =
+                '<span>0</span><span>0</span><span>0</span><span>0</span>';
+        }
 
     }, 1000);
 }
